cd571008136388a8bf95526fa4e7c8fd438a0671
AnastasiaGer <aagermanova@gmail.com>
2020-10-25 18:02:26 -0700

    arrows

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,21 @@
 $(function(){
+  $('.about__slider, .trip__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    speed: 300,
+    fade: true,
+    responsive: [{
+      breakpoint: 601,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
+
 
   $('.products__slider').slick({
     prevArrow: '<button class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></button>',
@@ -10,7 +27,7 @@ $(function(){
 
     $('.questions__item').removeClass('questions__item--active');
     $(this).parent().addClass('questions__item--active');
-    
+
   });
 
   $('#fullpage').fullpage({
@@ -27,7 +44,7 @@ $(function(){
     menu: '#header__nav',
     anchors: ['top', 'products', 'benefits', 'specification', 'questions','contacts'],
   });
-  
+
   $('.menu__btn').on('click', function(){
     $('.menu__btn').toggleClass('menu__btn--active');
     $('.menu__list').toggleClass('menu__list--active');
@@ -38,5 +55,5 @@ $(function(){
     $('.menu__list').removeClass('menu__list--active');
   });
 
-  
+
 });
