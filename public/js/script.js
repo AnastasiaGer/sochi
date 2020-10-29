b9dc4a4ef00026e82062c78cd1d08cc732a7c91e
AnastasiaGer <aagermanova@gmail.com>
2020-10-28 18:08:17 -0700

    fancybox

--- public/js/script.js
+++ public/js/script.js
@@ -63,13 +63,6 @@ $(function(){
     fade: true,
   });
 
-  $('.questions__item-title').on('click' , function(){
-
-    $('.questions__item').removeClass('questions__item--active');
-    $(this).parent().addClass('questions__item--active');
-
-  });
-
   $('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
